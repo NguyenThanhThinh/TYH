477b3d5e3f5ff329f4106e12ec4fc2ea96430c49
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-06-09 11:06:46 +0700

    add user for domain

---
+++ TYH/TYH.Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace TYH.Domain.Entities
+{
+    using TYH.Domain.Enums;
+    using TYH.Domain.Interfaces;
+    public class User : IdentityUser<Guid>, IDateTracking, ISwitchable
+    {
+        public User()
+        {
+
+        }
+
+        public User(Guid id, string fullName, string userName,
+             string email, string phoneNumber, string avatar, Status status)
+        {
+            Id = id;
+            FullName = fullName;
+            UserName = userName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Avatar = avatar;
+            Status = status;
+        }
+
+        public string FullName { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public decimal Balance { get; set; }
+        public string Avatar { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public Status Status { get; set; }
+    }
+}
