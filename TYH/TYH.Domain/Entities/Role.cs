d67bced1e28448b663f6e1cbbf0d7e0e7f4713f2
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-06-09 11:13:25 +0700

    add role

---
+++ TYH/TYH.Domain/Entities/Role.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TYH.Domain.Entities
+{
+    public class Role:IdentityRole<Guid>
+    {
+        public Role() : base()
+        {
+
+        }
+
+        public Role(string name,string description) : base(name)
+        {
+            this.Description = description;
+        }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+    }
+}
