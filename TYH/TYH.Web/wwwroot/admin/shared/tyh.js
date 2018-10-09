acadec507e346dc60c98b7aba31ab220f8d4a293
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-10-09 22:59:33 +0700

    add config js

---
+++ TYH/TYH.Web/wwwroot/admin/shared/tyh.js
@@ -0,0 +1,30 @@
+﻿var tyh = {
+    configs: {
+        pageSize: 10,
+        pageIndex: 1
+    },
+    notyfi: function (message, type) {
+
+    },
+    confirm: function (message, okCallBack) {
+
+    },
+    dateFomatJson: function (datetime) {
+
+    },
+    datetimeFomatJson: function (datetime) {
+
+    },
+    startLoading: function () {
+
+    },
+    stopLoading: function () {
+
+    },
+    getStatus: function (status) {
+
+    },
+    fomatNumber: function (number, precision) {
+
+    }
+}
