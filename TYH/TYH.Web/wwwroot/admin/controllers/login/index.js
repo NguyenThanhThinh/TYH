e5900d793e4afad3e4a1deaf53cb61b16728aab7
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-11 08:00:21 +0700

    add validation form

--- TYH/TYH.Web/wwwroot/admin/controllers/login/index.js
+++ TYH/TYH.Web/wwwroot/admin/controllers/login/index.js
@@ -1,43 +1,59 @@
-﻿var loginController = function () {
-    this.initialize = function () {
+﻿var loginController = function() {
+    this.initialize = function() {
         registerEvents();
-    }
+    };
 
-    var registerEvents = function () {
-        $('#btnLogin').on('click',
-            function (e) {
-                e.preventDefault();
-                var user = $('#txtUserName').val();
-                var password = $('#txtPassword').val();
-                login(user, password);
+    var registerEvents = function() {
+        $("#myform").validate({
+            errorClass: "red",
+            rules: {
+                userName: { required: true, minlength: 6 },
+                password: { required: true, minlength: 6 }
+
+            },
+            messages: {
+                userName: { required: "Chưa nhập tài khoản", minlength: "Tối thiểu 6 kí tự" },
+                password: { required: "Chưa nhập mật khẩu", minlength: "Tối thiểu 6 kí tự" }
+            }
+        });
+        $("#btnLogin").on("click",
+            function(e) {
+                if ($("#myform").valid()) {
+                    e.preventDefault();
+                    const user = $("#txtUserName").val();
+                    const password = $("#txtPassword").val();
+                    login(user, password);
+                }
             });
-        $(document).keypress(function (e) {
+        $(document).keypress(function(e) {
             if (e.which === 13) {
-                e.preventDefault();
-                var user = $('#txtUserName').val();
-                var password = $('#txtPassword').val();
-                login(user, password);
+                if ($("#myform").valid()) {
+                    e.preventDefault();
+                    const user = $("#txtUserName").val();
+                    const password = $("#txtPassword").val();
+                    login(user, password);
+                }
             }
         });
 
-    }
+    };
 
-    var login = function (user, pass) {
+    var login = function(user, pass) {
         $.ajax({
-            type: 'POST',
+            type: "POST",
             data: {
                 UserName: user,
                 Password: pass
             },
-            dateType: 'json',
-            url: '/admin/login/authen',
-            success: function (res) {
+            dateType: "json",
+            url: "/admin/login/authen",
+            success: function(res) {
                 if (res.success) {
                     window.location.href = "/Admin/Home/Index";
                 } else {
-                    tyh.notify('Đăng nhập không đúng', 'error');
+                    tyh.notify("Đăng nhập không đúng", "error");
                 }
             }
         });
-    }
+    };
 }
